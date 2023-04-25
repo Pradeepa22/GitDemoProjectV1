3d6e7e775e6f23d10dc7224f10ca6f78dbfcf38f
Pradeepa.Pandiyan <Pradeepa.Pandiyan@ACHIT179>
2023-04-25 16:29:45 +0530

    First Commit.....

---
+++ src/TestJava/Test1.java
@@ -0,0 +1,18 @@
+package TestJava;
+public class Test1 
+{
+	public static void main(String[] args) 
+	{
+
+		System.out.println("===================Email===================");
+		String hungry = "YES";
+		if(hungry=="YES")
+			System.out.println("...EAT SOMETHING...");
+		else
+			System.out.println("...DO SOME WORK...");
+		
+		System.out.println("===================Email===================");
+		
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Pradeepa.Pandiyan
+ *
+ */
+module GitDemoProjectV1 {
+}
